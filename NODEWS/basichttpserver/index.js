a94385451be6038c4a0bd75b5a475b741c5ff081
Abhay 076 <yadavabhaycs@gmail.com>
2022-09-24 09:19:41 +0530

    Extending to multiple pages

--- NODEWS/basichttpserver/index.js
+++ NODEWS/basichttpserver/index.js
@@ -5,13 +5,25 @@ function requestHandler(req,res){
     console.log(req.url);
     res.writeHead(200,{'content-type':'text/html'});
     // res.end('<h1>Hello World</h1>');
-    fs.readFile('./index.html',function(err,data){
-        if(err){
-            console.log('Error',err);
-            return res.end('<h1> Error</h1>');
-        }
-        return res.end(data);
-    })
+   let filePath;
+   switch(req.url){
+      case '/':
+        filePath ='./index.html'
+        break;
+      case '/profile':
+        filePath = './profile.html'
+        break;
+      default:
+        filePath='./404.html';
+        break;
+   }
+   fs.readFile(filePath,function(err,data){
+       if(err){
+        console.log('Error',err);
+        return res.end('<h1>Error</h1>');
+       }
+       return res.end(data);
+   });
 }
 const server = http.createServer(requestHandler);
 
